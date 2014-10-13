442a37266d9913e7b310466ff0ffde1ef7e271fb
m16a1 <a741su@gmail.com>
2014-10-13 18:39:15 +0400

    brand histogram

--- lib/Catalol/AspectHistogram.php
+++ lib/Catalol/AspectHistogram.php
@@ -9,6 +9,7 @@ class AspectHistogram
     private $aspects;
     private $aspectNames = [];
     private $selection = [];
+    private $brands = [];
 
     public function __construct(array $data)
     {
@@ -19,6 +20,7 @@ class AspectHistogram
         $this->aspects = new \ArrayIterator($aspects);
         $this->aspectNames = $data['other_params'];
         $this->selection = $data['selection'];
+        $this->brands = $data['brands'];
     }
 
     public function getAspects()
@@ -34,5 +36,10 @@ class AspectHistogram
     public function getSelection()
     {
         return $this->selection;
+    }
+
+    public function getBrands()
+    {
+        return $this->brands;
     }
 }
